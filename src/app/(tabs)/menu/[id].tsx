7ead6ebd4f997644cde86ec1e70dc1c6cad0036b
Justin <justinguerrerowork@gmail.com>
2024-03-27 17:20:59 -0400

    render and add cart item logic

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,11 +1,13 @@
-import {Stack,  useLocalSearchParams } from 'expo-router';
+import {Stack,  useLocalSearchParams, useRouter} from 'expo-router';
 import {View, Text, Image, StyleSheet, Pressable} from 'react-native';
 import products from '@/assets/data/products';
 import { defaultPizzaImage } from '@/src/components/ProductListItem';
 import { useState } from 'react';
-import Button from '@/src/components/Button'
+import Button from '@/src/components/Button';
+import { useCart } from '@/src/providers/CartProvider';
+import { PizzaSize } from '@/src/types';
 
-const sizes = ['S', 'M', 'L', 'XL'];
+const sizes: PizzaSize[]=  ['S', 'M', 'L', 'XL'];
 
 
 
@@ -14,10 +16,18 @@ const ProductDetailScreen = ()=>{
 
     const product = products.find((p) => p.id.toString() === id); 
 
-    const [selectedSize, setSelectedSize] = useState("M");
+    const {addItem} = useCart();
+
+    const router = useRouter();
+
+    const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
 
     const addToCart = () =>{
-        console.warn("Adding to cart, size: ", selectedSize);
+        if (!product){
+            return;
+        }
+        addItem(product, selectedSize);
+        router.push('/cart');
 
     }
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
+import CartProvider from '../providers/CartProvider';
 import { useColorScheme } from '@/src/components/useColorScheme';
 
 export {
@@ -49,10 +49,12 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <CartProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
       </Stack>
+      </CartProvider>
     </ThemeProvider>
   );
 }
