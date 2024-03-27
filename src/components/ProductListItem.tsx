2dce665497c9fef99286d9a07ebed0f44dd3f1d8
Justin <justinguerrerowork@gmail.com>
2024-03-26 20:14:28 -0400

    flatlist

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,51 +1,52 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../constants/Colors';
-import {Product} from "../types"
+import { Product } from '../types';
 
-export const defaultPizzaImage = 
-'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.default.png';
+export const defaultPizzaImage =
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 type ProductListItemProps = {
   product: Product;
+};
 
-}
-
-const ProductListItem = ({product}: ProductListItemProps) =>{
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style = {styles.container}>
-    <Image source = {{uri: product.image || defaultPizzaImage}} style = {styles.image}/>
-      
-    <Text style = {styles.title}>{product.name}</Text>
-    <Text style = {styles.price}>${product.price}</Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
-}
+};
 
 export default ProductListItem;
 
-
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor:'white',
-    padding:10,
-    borderRadius:20,
-
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
   },
- 
+
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical:10,
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
   },
-
   price: {
     color: Colors.light.tint,
-    fontWeight:'bold',
-
+    fontWeight: 'bold',
   },
-  image: {
-    width:'100%',
-    aspectRatio:1,
-    
-  }
 });
