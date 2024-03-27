4f3ddb195299ca89dbcfaa5b7b0dee39dc56018d
Justin <justinguerrerowork@gmail.com>
2024-03-26 20:40:50 -0400

    stack screens

---
+++ src/app/(tabs)/menu/[id].tsx
@@ -0,0 +1,14 @@
+import {Stack,  useLocalSearchParams } from 'expo-router';
+import {View, Text} from 'react-native';
+
+const ProductDetailScreen = ()=>{
+    const {id} = useLocalSearchParams();
+    return(
+        <View>
+            <Stack.Screen options = {{title:'Details'}}/>
+            <Text style = {{fontSize:20}}>ProductDetailsScreen for id : {id} </Text>
+        </View>
+    );
+};
+
+export default ProductDetailScreen;
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import Colors from '../constants/Colors';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 export const defaultPizzaImage =
   'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
@@ -11,7 +12,8 @@ type ProductListItemProps = {
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
+    <Link href = {`/menu/${product.id}`} asChild>
+    <Pressable style={styles.container}>
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
@@ -20,7 +22,10 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
-    </View>
+
+      
+    </Pressable>
+    </Link>
   );
 };
 
