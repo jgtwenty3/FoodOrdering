b1c14cf1f471d310bbf85a87dacd414c8322be6f
Justin <justinguerrerowork@gmail.com>
2024-03-29 13:38:49 -0400

    admin

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -7,12 +7,14 @@ type CartType = {
     items: CartItem[],
     addItem: (product: Product, size: CartItem['size']) => void;
     updateQuantity: (itemId:string, amount: -1 | 1)=>void;
+    total: number,
 };
 
  const CartContext = createContext<CartType>({
     items:[],
     addItem:()=>{},
-    updateQuantity: ()=>{}
+    updateQuantity: ()=>{}, 
+    total: 0,
 });
 
 const CartProvider = ({children}:PropsWithChildren)=>{
@@ -46,10 +48,12 @@ const CartProvider = ({children}:PropsWithChildren)=>{
             ).filter((item)=> item.quantity >0)
         );
     };
+
+    const total = items.reduce((sum, item)=> sum += item.product.price * item.quantity, 0);
     
 
     return(
-        <CartContext.Provider value = {{items, addItem, updateQuantity}}>
+        <CartContext.Provider value = {{items, addItem, updateQuantity, total}}>
     
             {children}
 
