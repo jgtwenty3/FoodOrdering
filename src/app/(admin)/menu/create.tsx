0b7983b5229b0eb6f35ac2e19868a9d4d221e7dc
Justin <justinguerrerowork@gmail.com>
2024-04-07 16:52:23 -0400

    storage

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -15,6 +15,7 @@ import {
 import * as FileSystem from 'expo-file-system';
 import { randomUUID } from 'expo-crypto';
 import { supabase } from '@/src/lib/supabase';
+import {decode} from 'base64-arraybuffer';
 
 
 const CreateProductScreen = () => {
@@ -80,11 +81,11 @@ const CreateProductScreen = () => {
       return;
     }
 
-    // const imagePath = await uploadImage();
+    const imagePath = await uploadImage();
 
-    // Save in the database
+  
     insertProduct(
-      { name, price: parseFloat(price), image },
+      { name, price: parseFloat(price), image: imagePath },
       {
         onSuccess: () => {
           resetFields();
@@ -94,6 +95,7 @@ const CreateProductScreen = () => {
     );
   };
 
+  
   const onUpdate = async () => {
     if (!validateInput()) {
       return;
@@ -135,6 +137,25 @@ const CreateProductScreen = () => {
     });
   };
 
+  const uploadImage = async () => {
+    if (!image?.startsWith('file://')) {
+      return;
+    }
+  
+    const base64 = await FileSystem.readAsStringAsync(image, {
+      encoding: 'base64',
+    });
+    const filePath = `${randomUUID()}.png`;
+    const contentType = 'image/png';
+    const { data, error } = await supabase.storage
+      .from('product-images')
+      .upload(filePath, decode(base64), { contentType });
+  
+    if (data) {
+      return data.path;
+    }
+  };
+
   const confirmDelete = () => {
     Alert.alert('Confirm', 'Are you sure you want to delete this product', [
       {
@@ -147,28 +168,6 @@ const CreateProductScreen = () => {
       },
     ]);
   };
-
-  // const uploadImage = async () => {
-  //   if (!image?.startsWith('file://')) {
-  //     return;
-  //   }
-
-  //   const base64 = await FileSystem.readAsStringAsync(image, {
-  //     encoding: 'base64',
-  //   });
-  //   const filePath = `${randomUUID()}.png`;
-  //   const contentType = 'image/png';
-
-  //   const { data, error } = await supabase.storage
-  //     .from('product-images')
-  //     .upload(filePath, decode(base64), { contentType });
-
-  //   console.log(error);
-
-  //   if (data) {
-  //     return data.path;
-  //   }
-  // };
 
   return (
     <View style={styles.container}>
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View, Image, Pressable, } from 'react-native';
 import Colors from '../constants/Colors';
 import { Link, useSegments } from 'expo-router';
-import { Tables } from '../database.types';
+import { Tables } from '../types';
+import RemoteImage from './RemoteImage';
 
 export const defaultPizzaImage =
   'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
@@ -16,10 +17,11 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <Link href = {`/${segments[0]}/menu/${product.id}`} asChild>
     <Pressable style={styles.container}>
-      <Image
-        source={{ uri: product.image || defaultPizzaImage }}
-        style={styles.image}
-        resizeMode="contain"
+      <RemoteImage
+       path = {product.image}
+       fallback = {defaultPizzaImage}
+       style={styles.image}
+       resizeMode="contain"
       />
 
       <Text style={styles.title}>{product.name}</Text>
